cb450589cb479e9062cda4bffb6b61d564be51f3
Graham Watson <12705535+Goombario@users.noreply.github.com>
2016-10-24 19:21:35 -0400

    Changed object loading to use the new function Added texture binding function (unknown if done right) - Graham

--- src/objLoader.h
+++ src/objLoader.h
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <vector>
 #include <Windows.h>
+#include <gl\glew.h>
 #include <glm\glm.hpp>
 
 class uv
@@ -21,17 +22,19 @@ public:
 	std::vector<glm::vec2> const& getUV() const;
 	std::vector<glm::vec3> const& getNormal() const;
 	std::vector<float> const& getColor() const;
-
 	void setVertex(int index, glm::vec3 newVertex);
-private:
-	void bindObjectData(int VAOindex);
+	void bindObjectData(GLuint DrawType = GL_DYNAMIC_DRAW);
+	void bindTexture(char* filePath);
 
+private:
+	bool isEnvironment;
 	std::vector<glm::vec3> out_vertices;
 	std::vector<glm::vec2> out_uvs;
 	std::vector<glm::vec3> out_normals;
 	std::vector<float> color;
-	std::vector<BYTE> data;
-	UINT uiVAO[1];
-	UINT uiVBO[2];
-	UINT uiBuffer;
+	//std::vector<BYTE> data;
+	GLuint uiVAO[1];
+	GLuint uiVBO[3];
+	GLuint texHandle[1];
+	GLuint texSampler[1];
 };
