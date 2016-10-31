a0dae88bf01b4a858cddaa572c5ee2613b8dc84c
Goombario <Goombario@localhost>
2016-10-31 16:46:27 -0400

    Added animation class and AnimatedObject class FIX - errors with LERP, testing, reading from file

--- src/objLoader.h
+++ src/objLoader.h
@@ -17,17 +17,37 @@ class Loader
 public:
 	Loader();
 	Loader(char const* filePath);
+
 	bool load(char const* filePath);
-	std::vector<glm::vec3> const& getVertex() const;
-	std::vector<glm::vec2> const& getUV() const;
-	std::vector<glm::vec3> const& getNormal() const;
-	std::vector<float> const& getColor() const;
-	void setVertex(int index, glm::vec3 newVertex);
+	std::vector<glm::vec3> const& getVertices() const;
+	std::vector<glm::vec2> const& getUVs() const;
+	std::vector<glm::vec3> const& getNormals() const;
+	std::vector<float> const& getColors() const;
+	void setVertex(int index, glm::vec3 const& newVertex);
+	void setVertices(std::vector<glm::vec3> const& newVertices);
 
 private:
-	bool isEnvironment;
 	std::vector<glm::vec3> out_vertices;
 	std::vector<glm::vec2> out_uvs;
 	std::vector<glm::vec3> out_normals;
 	std::vector<float> color;
+};
+
+// Animation class used to control flow of objects and speed
+class Animation
+{
+public:
+	Animation();
+	Animation(const char* filePath);
+	Animation(std::vector<Loader> const&);
+	
+	void update(float deltaTime, Loader & base);
+	void reset();
+
+private:
+	std::vector<Loader> poseList;
+	int currentPose, nextPose;
+	
+	// To put speed control in here
+	float time;
 };
